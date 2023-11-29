736699e0c4d91241b897373c274582633cbaa57e
nikolaprljeta <nikola.prljeta@gmail.com>
2023-11-29 12:17:57 +0100

    On my way

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import ListScreen from './src/screens/ListScreen';
 import DetailScreen from './src/screens/DetailScreen';
 
--- src/components/ReusableView.jsx
+++ src/components/ReusableView.jsx
@@ -1,48 +1,37 @@
 import React from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const ReusableView = ({onPress, title, description, additionalInfo}) => {
-    return (
-        <TouchableOpacity onPress={onPress}
-            style={
-                styles.container
-        }>
-            <Text style={
-                styles.title
-            }>
-                {title}</Text>
-            <Text style={
-                styles.description
-            }>
-                {description}</Text>
-            <Text style={
-                styles.additionalInfo
-            }>
-                {additionalInfo}</Text>
-        </TouchableOpacity>
-    );
+const ReusableView = ({ onPress, title, description, additionalInfo }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Text style={styles.additionalInfo}>{additionalInfo}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 8,
-        padding: 16,
-        marginBottom: 16
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 8
-    },
-    description: {
-        textAlign: 'left',
-        marginBottom: 8
-    },
-    additionalInfo: {
-        color: '#666'
-    }
+  container: {
+    backgroundColor: '#fff', // Set the background color to white
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  description: {
+    textAlign: 'left',
+    marginBottom: 8,
+  },
+  additionalInfo: {
+    color: '#666',
+  },
 });
 
 export default ReusableView;
